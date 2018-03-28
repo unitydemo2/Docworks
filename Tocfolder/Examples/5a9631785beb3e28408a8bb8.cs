38945c39611a0bdc1f0511b4a18aedcec36541ec
unitydemo2 <unitydemo2@yopmail.com>
2018-03-28 15:09:24 +0530

    DW CMS Message

---
+++ Tocfolder/Examples/5a9631785beb3e28408a8bb8.cs
@@ -0,0 +1,15 @@
+to create a prjct in soap ui first click on file 
+* new soap ui-> copy and paste the URL given by developers in initial WSDL.
+* prjct name anything we have given.
+* prjct->interface-> features(methods).
+* each and every method is  associated with request
+
+
+
+To create a test suite right click in features->create new test suite-> rename it
+inside test suite->test case-> 3things-> test steps
+                                         load steps,security steps.
+										 
+inside test step-> right click-> add step		
+ service provider creates the WSDL and service consumer gets the WSDl and uses for getting the response.
+Server directly provides the URL to the WSDL and the client uses the response directly								 
