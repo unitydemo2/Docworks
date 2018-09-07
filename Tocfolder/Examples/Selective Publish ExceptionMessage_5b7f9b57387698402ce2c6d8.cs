f6092519ddf285f800872c67ce421cd511efc1a6
unitydemo2 <unitydemo2@yopmail.com>
2018-09-07 21:33:54 +0530

    DW CMS Message

---
+++ Tocfolder/Examples/Selective Publish ExceptionMessage_5b7f9b57387698402ce2c6d8.cs
@@ -0,0 +1,403 @@
+An error has occured, please send the following to an administrator
+    {
+  "content": {
+    "distributionId": "5b5e9e5f88950427c8fcf4d8",
+    "selectedNodeIdList": [
+      "5b5e9e8188950427c8fcf4da",
+      "5b5e9e8188950427c8fcf4dc"
+    ],
+    "excludedTagIdList": [],
+    "publishedBy": "5ac1c65090dcce2ef0220b28",
+    "publishedDate": 1533114065,
+    "userId": "5ac1c65090dcce2ef0220b28",
+    "sedaOperation": 37,
+    "publishHistoryId": "5b6176d4be4e2b08ac5d874b",
+    "selectedNodeForPublishList": [
+      {
+        "projectId": "5b586e7588950427c8fcefe1",
+        "distributionId": "5b5e9e5f88950427c8fcf4d8",
+        "nodeId": "5b5e9e8188950427c8fcf4da",
+        "title": "Unity User Manual ",
+        "tags": [],
+        "documentOperation": 10,
+        "draftSnapshotId": "5b5e9e9c88950427c8fcf4ee",
+        "createdBy": "5ac1c65090dcce2ef0220b28",
+        "_id": "5b5ea0ff88950427c8fcf527",
+        "status": 2,
+        "modifiedDate": 1532928255,
+        "createdDate": 1532928255
+      },
+      {
+        "projectId": "5b586e7588950427c8fcefe1",
+        "distributionId": "5b5e9e5f88950427c8fcf4d8",
+        "nodeId": "5b5e9e8188950427c8fcf4dc",
+        "title": "Unity User Manual ",
+        "tags": [],
+        "documentOperation": 1,
+        "draftSnapshotId": "5b5e9e9d88950427c8fcf4fa",
+        "createdBy": "5ac1c65090dcce2ef0220b28",
+        "_id": "5b5e9ea388950427c8fcf51c",
+        "status": 2,
+        "modifiedDate": 1532927651,
+        "createdDate": 1532927651
+      }
+    ],
+    "project": {
+      "projectName": "TestProject_GitHub",
+      "repositoryId": 118083636,
+      "repositoryName": "Docworks",
+      "description": "",
+      "typeOfContent": 3,
+      "publishedPath": "published path",
+      "tagGroups": [
+        "5afd21b3d1fb6547a0d266d9",
+        "5b0e58ecefcd013198d41efc",
+        "5b11558defcd013198d4509d",
+        "5b1155a9efcd013198d450a0",
+        "5b1155b4efcd013198d450a1",
+        "5b1155beefcd013198d450a2",
+        "5b1155c9efcd013198d450a3",
+        "5b1155e2efcd013198d450a6",
+        "5b1208c3efcd013198d4789f",
+        "5b1208deefcd013198d478a2",
+        "5b1208e9efcd013198d478a5",
+        "5b1208f4efcd013198d478a8",
+        "5b1208ffefcd013198d478aa",
+        "5b120918efcd013198d478b7",
+        "5b14c5ccefcd013198d493ca",
+        "5b1590a0efcd013198d49c43",
+        "5b1590beefcd013198d49c46",
+        "5b1590c9efcd013198d49c47",
+        "5b1590d5efcd013198d49c48",
+        "5b1590e0efcd013198d49c49",
+        "5b15d5baefcd013198d49c4a",
+        "5b1657d0efcd013198d4b6b4",
+        "5b16591defcd013198d4b708",
+        "5b165993efcd013198d4b747",
+        "5b165a0aefcd013198d4b76d",
+        "5b165b7eefcd013198d4b7f4",
+        "5b165bfbefcd013198d4b845",
+        "5b165cd8efcd013198d4b857",
+        "5b1667acefcd013198d4bbea",
+        "5b166801efcd013198d4bbf0",
+        "5b16d4d4efcd013198d4cc92",
+        "5b16d4e8efcd013198d4cc94",
+        "5b16d4f2efcd013198d4cc95",
+        "5b16d4fcefcd013198d4cc97",
+        "5b16d506efcd013198d4cc98",
+        "5b16d598efcd013198d4cca6",
+        "5b1750c4d3b87811247514df",
+        "5b1770cb66d6340e2816a4a5",
+        "5b182b1166d6340e2816b402",
+        "5b19191b1462950d280dd9df",
+        "5b1919341462950d280dd9fc",
+        "5b19193f1462950d280dda00",
+        "5b19194a1462950d280dda01",
+        "5b1919551462950d280dda03",
+        "5b1919731462950d280dda06",
+        "5b1a8af51462950d280e106f",
+        "5b1aa7411462950d280e1322",
+        "5b1aa7571462950d280e1324",
+        "5b1aa7611462950d280e1325",
+        "5b1aa76c1462950d280e1326",
+        "5b1aa7771462950d280e1327",
+        "5b1aa78f1462950d280e132a",
+        "5b1f4ec7f79c2e1ca0fd4d29",
+        "5b2640c0966bb00600c06c94",
+        "5b2640d4966bb00600c06c96",
+        "5b2640df966bb00600c06c97",
+        "5b2640ea966bb00600c06c98",
+        "5b2640f4966bb00600c06c99",
+        "5b264114966bb00600c06c9c",
+        "5b29d515776dd513a0459d4e",
+        "5b341bd9548ea10fd8ae6f56",
+        "5b341bed548ea10fd8ae6f58",
+        "5b341bf7548ea10fd8ae6f59",
+        "5b341c01548ea10fd8ae6f5a",
+        "5b341c0b548ea10fd8ae6f5b",
+        "5b341c2a548ea10fd8ae6f5e",
+        "5b3426a9548ea10fd8ae72c0",
+        "5b3426bd548ea10fd8ae72cb",
+        "5b3426c7548ea10fd8ae72cc",
+        "5b3426d1548ea10fd8ae72ce",
+        "5b3426db548ea10fd8ae72eb",
+        "5b342789548ea10fd8ae7306",
+        "5b3427e5548ea10fd8ae731f",
+        "5b3427fb548ea10fd8ae7321",
+        "5b342805548ea10fd8ae7322",
+        "5b34280f548ea10fd8ae7323",
+        "5b342819548ea10fd8ae7324",
+        "5b342839548ea10fd8ae732a",
+        "5b3791fc548ea10fd8aecaa1",
+        "5b44705fa8a5780b043b57ed",
+        "5b447077a8a5780b043b57ef",
+        "5b447083a8a5780b043b57f0",
+        "5b44708ea8a5780b043b57f1",
+        "5b44709aa8a5780b043b57f2",
+        "5b4470bda8a5780b043b57f5",
+        "5b45847ca8a5780b043b60ae",
+        "5b458497a8a5780b043b60eb",
+        "5b4584a5a8a5780b043b60f3",
+        "5b4584b1a8a5780b043b60f5",
+        "5b4584bda8a5780b043b60f6",
+        "5b4584e0a8a5780b043b60f9",
+        "5b459454a8a5780b043b65b7",
+        "5b45946ea8a5780b043b65db",
+        "5b45947aa8a5780b043b65dd",
+        "5b459486a8a5780b043b65df",
+        "5b459492a8a5780b043b65e0",
+        "5b4594b7a8a5780b043b65e6",
+        "5b4666d8a8a5780b043b752d",
+        "5b466887a8a5780b043b7596",
+        "5b47442dfc232014385f7720",
+        "5b474449fc232014385f7722",
+        "5b474456fc232014385f7723",
+        "5b474464fc232014385f7724",
+        "5b474473fc232014385f7725",
+        "5b474499fc232014385f7728",
+        "5b481c661e8fb8132c4c0b70",
+        "5afe687cd0d202135c81e842",
+        "5afe68a7d0d202135c81e844"
+      ],
+      "sourceControlType": 1,
+      "mercurialRepositoryUrl": "",
+      "isSourceSyncRequired": true,
+      "sourceControlProviderType": 2,
+      "projectAdmins": [
+        {
+          "userId": "5ac1cc4aabb4982a40a134ce",
+          "assignedBy": "5ac1c65090dcce2ef0220b28",
+          "assignedOn": 1532952936
+        },
+        {
+          "userId": "5ac30be4abb4982a40a148f6",
+          "assignedBy": "5ac1c65090dcce2ef0220b28",
+          "assignedOn": 1532952953
+        }
+      ],
+      "_id": "5b586e7588950427c8fcefe1",
+      "status": 2,
+      "modifiedDate": 1532952953,
+      "createdDate": 1532522101
+    },
+    "distribution": {
+      "projectId": "5b586e7588950427c8fcefe1",
+      "distributionName": "GitHub_Distribution",
+      "branchName": "DocWorksManual3",
+      "description": null,
+      "isDefault": false,
+      "isActive": true,
+      "tocPath": "TocFolder",
+      "lastAcceptToLiveDate": 1533113981,
+      "branchNameCounter": 0,
+      "docWorksBranchName": "DW/DocWorksManual3/testproject_github/github_distribution/Manual-0",
+      "_id": "5b5e9e5f88950427c8fcf4d8",
+      "status": 2,
+      "modifiedDate": 1533113981,
+      "createdDate": 1532927583
+    },
+    "typeOfContent": 3,
+    "distributionSnapshotDataList": [
+      {
+        "nodeId": "5b5e9e8188950427c8fcf4d9",
+        "shortTitle": "UnityManual",
+        "parentId": null,
+        "draftSnapshotId": null,
+        "assignedTags": [],
+        "tags": [],
+        "fileName": "",
+        "nodeOrderIndex": 0
+      },
+      {
+        "nodeId": "5b5e9e8188950427c8fcf4da",
+        "shortTitle": "UnityManual Overview",
+        "parentId": "5b5e9e8188950427c8fcf4d9",
+        "draftSnapshotId": "5b5e9e9c88950427c8fcf4ee",
+        "assignedTags": [
+          "5afe68bad0d202135c81e845",
+          "5afe68e5d0d202135c81e84a",
+          "5afd21b4d1fb6547a0d266db"
+        ],
+        "tags": [
+          "5afe68bad0d202135c81e845",
+          "5afe68e5d0d202135c81e84a",
+          "5afd21b4d1fb6547a0d266db"
+        ],
+        "fileName": "UnityManualaa.md",
+        "nodeOrderIndex": 0
+      },
+      {
+        "nodeId": "5b5e9e8188950427c8fcf4db",
+        "shortTitle": "UnityManual",
+        "parentId": "5b5e9e8188950427c8fcf4d9",
+        "draftSnapshotId": null,
+        "assignedTags": [],
+        "tags": [],
+        "fileName": "",
+        "nodeOrderIndex": 0
+      },
+      {
+        "nodeId": "5b5e9e8188950427c8fcf4dc",
+        "shortTitle": "UnityManual Overview",
+        "parentId": "5b5e9e8188950427c8fcf4db",
+        "draftSnapshotId": "5b5e9e9d88950427c8fcf4fa",
+        "assignedTags": [
+          "5afe68e5d0d202135c81e84a",
+          "5afe68c5d0d202135c81e846"
+        ],
+        "tags": [
+          "5afe68e5d0d202135c81e84a",
+          "5afe68c5d0d202135c81e846"
+        ],
+        "fileName": "UnityManual_1.md",
+        "nodeOrderIndex": 0
+      }
+    ],
+    "hasNodesForPublish": true
+  },
+  "userId": "5ac1c65090dcce2ef0220b28",
+  "flowMap": {
+    "sedaOperation": 37,
+    "eventGroups": [
+      [
+        {
+          "eventName": 1062,
+          "eventLocationInFlowMap": null,
+          "status": 2,
+          "childSedaOperation": 0,
+          "childOperationStatus": null,
+          "eventCondition": null,
+          "reuseSedaOperation": 0,
+          "reuseNotificationExpression": false,
+          "conditionKeys": null,
+          "requestMapProperty": null,
+          "responseMapProperty": null
+        }
+      ],
+      [
+        {
+          "eventName": 1107,
+          "eventLocationInFlowMap": null,
+          "status": 2,
+          "childSedaOperation": 0,
+          "childOperationStatus": null,
+          "eventCondition": null,
+          "reuseSedaOperation": 0,
+          "reuseNotificationExpression": false,
+          "conditionKeys": null,
+          "requestMapProperty": null,
+          "responseMapProperty": null
+        }
+      ],
+      [
+        {
+          "eventName": 1064,
+          "eventLocationInFlowMap": null,
+          "status": 2,
+          "childSedaOperation": 0,
+          "childOperationStatus": null,
+          "eventCondition": null,
+          "reuseSedaOperation": 0,
+          "reuseNotificationExpression": false,
+          "conditionKeys": null,
+          "requestMapProperty": null,
+          "responseMapProperty": null
+        }
+      ],
+      [
+        {
+          "eventName": 1065,
+          "eventLocationInFlowMap": null,
+          "status": 3,
+          "childSedaOperation": 0,
+          "childOperationStatus": null,
+          "eventCondition": "Convert.ToBoolean(HasNodesForPublish)==true",
+          "reuseSedaOperation": 0,
+          "reuseNotificationExpression": false,
+          "conditionKeys": [
+            "HasNodesForPublish"
+          ],
+          "requestMapProperty": null,
+          "responseMapProperty": null
+        }
+      ],
+      [
+        {
+          "eventName": 5003,
+          "eventLocationInFlowMap": null,
+          "status": 0,
+          "childSedaOperation": 0,
+          "childOperationStatus": null,
+          "eventCondition": "Convert.ToBoolean(HasNodesForPublish)==true",
+          "reuseSedaOperation": 0,
+          "reuseNotificationExpression": false,
+          "conditionKeys": [
+            "HasNodesForPublish"
+          ],
+          "requestMapProperty": null,
+          "responseMapProperty": null
+        }
+      ],
+      [
+        {
+          "eventName": 1066,
+          "eventLocationInFlowMap": null,
+          "status": 0,
+          "childSedaOperation": 0,
+          "childOperationStatus": null,
+          "eventCondition": "Convert.ToBoolean(HasNodesForPublish)==true",
+          "reuseSedaOperation": 0,
+          "reuseNotificationExpression": false,
+          "conditionKeys": [
+            "HasNodesForPublish"
+          ],
+          "requestMapProperty": null,
+          "responseMapProperty": null
+        }
+      ]
+    ],
+    "notificationType": 2,
+    "notificationTopic": "PublishDistribution",
+    "notificationExpression": [
+      "Topics/PublishHistory"
+    ],
+    "notificationExpressionWithType": [
+      {
+        "key": "Broadcast",
+        "value": "Topics/PublishHistory"
+      }
+    ],
+    "authorizationParameters": [
+      {
+        "distributionId": "DistributionId"
+      }
+    ],
+    "authorizationRequestType": "RequestBody",
+    "parentResponseId": null,
+    "parentEventLocation": null,
+    "isOperationCompleteWithStatusSuccess": false,
+    "_id": "5b58165cce97b460203ebbe1",
+    "status": 0,
+    "modifiedDate": 1532499548,
+    "createdDate": 1532499548
+  },
+  "createdOn": "2018-08-01T09:01:05.367Z",
+  "errorResponse": {
+    "modelValidationErrors": null,
+    "message": "Error occured in execution of: ",
+    "exceptionDetail": {
+      "exceptionMessage": "One or more errors occurred. (Length cannot be less than zero.\r\nParameter name: length)",
+      "stackTrace": "   at System.Threading.Tasks.TaskReplicator.Run[TState](ReplicatableUserAction`1 action, ParallelOptions options, Boolean stopOnFirstFailure)\r\n   at System.Threading.Tasks.Parallel.ForWorker[TLocal](Int32 fromInclusive, Int32 toExclusive, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, Func`4 bodyWithLocal, Func`1 localInit, Action`1 localFinally)\r\n--- End of stack trace from previous location where exception was thrown ---\r\n   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()\r\n   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw(Exception source)\r\n   at System.Threading.Tasks.Parallel.ThrowSingleCancellationExceptionOrOtherException(ICollection exceptions, CancellationToken cancelToken, Exception otherException)\r\n   at System.Threading.Tasks.Parallel.ForWorker[TLocal](Int32 fromInclusive, Int32 toExclusive, ParallelOptions parallelOptions, Action`1 body, Action`2 bodyWithState, Func`4 bodyWithLocal, Func`1 localInit, Action`1 localFinally)\r\n   at System.Threading.Tasks.Parallel.For(Int32 fromInclusive, Int32 toExclusive, Action`1 body)\r\n   at DocWorks.Core.EventHandler.DistributionSnapshotToTreeEventHandler.ProcessMap(Dictionary`2 map, DistributionSnapshotToTreeResponseModel root, String key) in C:\\projects\\docworks-core-v7fuu\\DocWorks.Core\\EventHandler\\DistributionSnapshotToTreeEventHandler.cs:line 36\r\n   at DocWorks.Core.EventHandler.DistributionSnapshotToTreeEventHandler.<Handle>d__0.MoveNext() in C:\\projects\\docworks-core-v7fuu\\DocWorks.Core\\EventHandler\\DistributionSnapshotToTreeEventHandler.cs:line 25\r\n--- End of stack trace from previous location where exception was thrown ---\r\n   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()\r\n   at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   at System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   at DocWorks.Common.SEDAEvents.Implementation.DefaultEventBusServiceBusMessageProcessor.<ExecuteEventHandlerAsync>d__9.MoveNext()\r\n--- End of stack trace from previous location where exception was thrown ---\r\n   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()\r\n   at System.Runtime.CompilerServices.TaskAwaiter.ThrowForNonSuccess(Task task)\r\n   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)\r\n   at System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()\r\n   at DocWorks.Common.SEDAEvents.Implementation.DefaultEventBusServiceBusMessageProcessor.<ProcessMessageAsync>d__7.MoveNext()",
+      "innerExceptionMessage": null,
+      "innerExceptionStackTrace": null
+    },
+    "httpStatusCode": null,
+    "errorCode": 0
+  },
+  "completedEventCount": 3,
+  "totalEventCount": 6,
+  "_id": "5b6176d185457d04787d793c",
+  "status": 3,
+  "modifiedDate": 1533114078,
+  "createdDate": 1533114065
+}
