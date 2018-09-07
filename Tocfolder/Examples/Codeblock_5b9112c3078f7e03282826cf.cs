27324648be7f107a40e01edab40eb013acc25fb3
unitydemo2 <unitydemo2@yopmail.com>
2018-09-07 10:55:02 +0530

    DW CMS Message

---
+++ Tocfolder/Examples/Codeblock_5b9112c3078f7e03282826cf.cs
@@ -0,0 +1,4 @@
+DocworksCmsQA.CustomException.AssertException: 
+'Expected : Upsertasset D:\CMS-parallel\parallel\cms\DocworksCmsQA\MediaFiles\Images\13-07-2018 12_34_50.jpg is successful   
+Actual :Upsert asset 06-09-2018 10_34_50 is successful,
+ERROR : Image Uploaded with status : upsert asset 14-07-2018 10_34_50 is successful'
