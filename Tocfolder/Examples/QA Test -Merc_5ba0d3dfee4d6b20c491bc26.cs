80aa0b0fd4c67a1523dd64db9a19e948e4ee5193
unitydemo2 <unitydemo2@yopmail.com>
2018-09-29 16:28:20 +0530

    DW CMS Message

---
+++ Tocfolder/Examples/QA Test -Merc_5ba0d3dfee4d6b20c491bc26.cs
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<doc version="3">
+    <member name="QA - CurveModifiedType" type="Enum" namespace="UnityEditor">
+        <section>
+            <summary>Describes the type of modification that caused OnCurveWasModified to fire cmsqatest summary.</summary>
+        </section>
+		 <section>
+            <description>Describes the type of modification that caused OnCurveWasModified to fire cmsqatest description.</description>
+        </section>
+        <member name="QA - CurveModifiedType.ClipModified123" type="EnumValue" />
+		    <section>
+                <summary>Describe the type of modification that caused ClipModified to fire.</summary>
+            </section>
+        <member name="AnimationUtility.CurveModifiedType.CurveDeleted" type="EnumValue" />
+        <member name="AnimationUtility.CurveModifiedType.CurveModified" type="EnumValue" />
+    </member>
+</doc>
